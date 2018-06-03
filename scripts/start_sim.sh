10b1b4b47d8e2267ede1835448739dfd696007f7
Yashren Reddi <yashren.reddi@uct.ac.za>
2018-06-03 16:33:18 +0200

    update

--- scripts/start_sim.sh
+++ scripts/start_sim.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-dronekit-sitl copter --home=-34.224470,19.128235,20,200 --model=quad param set SYSID_THISMAV 2
+# start simulators
+echo "Start simulators"
+gnome-terminal -e "dronekit-sitl copter --home=-34.224405,19.127213,0,200 --model=quad --instance 1"
+gnome-terminal -e "dronekit-sitl copter --home=-34.224395,19.127373,0,200 --model=quad --instance 2"
+# start mav proxy sessions
+sleep(5)
+echo "Start MAVproxy sessions"
+gnome-terminal -e "mavproxy.py --master=tcp:0.0.0.0:5770 --out=udp:0.0.0.0:14550"
+gnome-terminal -e "mavproxy.py --master=tcp:0.0.0.0:5780 --out=udp:0.0.0.0:14551"
 
